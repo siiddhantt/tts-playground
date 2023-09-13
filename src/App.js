21df2ddadee4d206e673b9d44e9de67794a6bf08
siiddhantt <siddhant.rai.5686@gmail.com>
2023-09-13 21:25:56 +0530

    fix: route updates

--- src/App.js
+++ src/App.js
@@ -1,103 +1,17 @@
-import { useState } from "react";
-import { TypeAnimation } from "react-type-animation";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { TTS_API } from "./api";
 
 import "./App.css";
+import Home from "./components/Home";
 import Form from "./components/Form";
 
 function App() {
-  const [flag, setFlag] = useState(false);
-  const [name, setName] = useState("");
-  const [isSubmit, setIsSubmit] = useState(false);
-  const handleButtonClick = async () => {
-    try {
-      const response = await TTS_API.brianTTS(
-        `Hello, ${name}! Welcome to our website. We're glad to have you here.`
-      );
-      if (response.status !== 200) {
-        const errorText = await response.text();
-        alert(errorText);
-        return;
-      }
-      const mp3 = await response.blob();
-      const blobUrl = URL.createObjectURL(mp3);
-      const audio = new Audio(blobUrl);
-      audio.pause();
-      audio.load();
-      audio.play();
-    } catch (error) {
-      console.error("An error occurred:", error);
-    }
-  };
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/form" element={<Form />} />
         </Routes>
-        <div className="head">
-          <div>
-            {flag ? (
-              <div>Hello World!👋</div>
-            ) : (
-              <TypeAnimation
-                cursor={false}
-                sequence={[
-                  `Hello World!👋`,
-                  1000,
-                  () => {
-                    setFlag(true);
-                  },
-                ]}
-                speed={10}
-              />
-            )}
-          </div>
-        </div>
-        <div className="middle">
-          <div>
-            {flag ? (
-              <>
-                <div>Hi there!👀 What's your name?</div>
-                <input
-                  className="name-field"
-                  value={name}
-                  onChange={(e) => {
-                    setName(e.target.value);
-                    setIsSubmit(false);
-                  }}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      setIsSubmit(true);
-                      handleButtonClick();
-                    }
-                  }}
-                ></input>
-              </>
-            ) : (
-              <></>
-            )}
-          </div>
-        </div>
-        <div className="body">
-          {isSubmit ? (
-            <div className="body-text">
-              <TypeAnimation
-                sequence={[
-                  `Hello, ${name}! Welcome to our website. We're glad to have you here.`,
-                  1000,
-                  () => {
-                    setFlag(true);
-                  },
-                ]}
-                speed={20}
-              ></TypeAnimation>
-            </div>
-          ) : (
-            <></>
-          )}
-        </div>
       </BrowserRouter>
     </div>
   );
